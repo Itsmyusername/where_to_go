8d96f6b8737f0468d128d0b4680247ff2f17c45b
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2024-01-24 00:25:40 +0300

    Add model for images and edit Place model

--- places/models.py
+++ places/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 
 class Place(models.Model):
-    name = models.TextField()
+    title = models.CharField(max_length=300)
+    description_short = models.TextField(blank=True)
+    description_long = models.TextField(blank=True)
+    lat = models.FloatField(default=0.0)
+    lon = models.FloatField(default=0.0)
 
     def __str__(self):
-        return self.name
+        return self.title
+
+
+class Image(models.Model):
+    img = models.ImageField()
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    position = models.PositiveSmallIntegerField(verbose_name='Позиция фото',
+                                                default=0,
+                                                blank=True)
+
+    def __str__(self):
+        return f'{self.position}, {self.place}'

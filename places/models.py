85688cc08da9de10060cd2e4eaeb1d406ff05134
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2024-01-28 22:56:11 +0300

    Move data from template to view

--- places/models.py
+++ places/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Place(models.Model):
     title = models.CharField(max_length=300)
     description_short = models.TextField(blank=True)
--- places/views.py
+++ places/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-# Create your views here.
+from .models import Place
+
+
+def main_page(request):
+    context = {
+        'data': {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [37.62, 55.793676]
+                    },
+                    "properties": {
+                        "title": "«Легенды Москвы",
+                        "placeId": "moscow_legends",
+                        "detailsUrl": 'static/places/moscow_legends.json'
+
+                    }
+                },
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [37.64, 55.753676]
+                    },
+                    "properties": {
+                        "title": "Крыши24.рф",
+                        "placeId": "roofs24",
+                        "detailsUrl": "static/places/roofs24.json"
+                    }
+                }
+            ]
+        }
+    }
+    return render(request, 'index.html', context=context)
